8876d2eaf017fddae99dca553f450b90a6ab59db
bluu123 <65077501+bluu123@users.noreply.github.com>
2022-06-13 12:50:38 +0300

    Update calc.java

--- src/calc.java
+++ src/calc.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class calc {
     public static void main(String[] args) {
         int a = 0, b = 0, c = 0;
-        boolean q1 = false, q2 = false, q3 = false;
+        boolean q1 = false, q2 = false;
         String[] roman = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
                 "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
                 "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
@@ -15,7 +15,6 @@ public class calc {
         String sa, sb;
         Scanner in = new Scanner(System.in);
         String op = in.nextLine();
-        String qq = " ";
         String[] strings = op.split(" ");
         if (strings.length == 3) {
         sa = strings[0];
@@ -65,10 +64,10 @@ public class calc {
                 System.out.println(roman[c]);
             }else {System.out.println("Ошибка");
             }
-            if ((q1 == false) && (q2 == false)&&(q3 = false)) {
+            if ((q1 == false) && (q2 == false)) {
                 System.out.println(c);
             }
 
-        }else {System.out.println("Ошибка");}
+        }
     }
 }
